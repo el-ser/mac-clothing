43c1a8ffc77e9c17c040d81624f37cedb3c31937
Macky Bugarin <manuel.bugarin@paymaya.com>
2022-06-02 21:09:58 +0800

    Added in add to cart feature

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart.context";
+
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
+import "./cart-dropdown.styles.scss";
+
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  console.log(cartItems);
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item}></CartItem>
+        ))}
+      </div>
+      <Button>GOT TO CHECKOUT</Button>
+    </div>
+  );
+};
+
+export default CartDropdown;
