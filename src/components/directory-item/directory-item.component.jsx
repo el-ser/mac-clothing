159dbd8403881888631d6fe1d324d65d2ea17970
Macky Bugarin <manuel.bugarin@paymaya.com>
2022-06-04 13:43:33 +0800

    Fixed styling issue

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,15 +1,15 @@
-import "./category-item.styles.scss";
+import "./directory-item.styles.scss";
 
-const CategoryItem = ({ category: { imageUrl, title } }) => {
+const DirectoryItem = ({ category: { imageUrl, title } }) => {
   return (
-    <div className="category-container">
+    <div className="directory-item-container">
       <div
         className="background-image"
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className="category-body-container">
+      <div className="body">
         <h2>{title}</h2>
         <p>Show now</p>
       </div>
@@ -17,4 +17,4 @@ const CategoryItem = ({ category: { imageUrl, title } }) => {
   );
 };
 
-export default CategoryItem;
+export default DirectoryItem;
